a3411415d090a41d785b5bf4df76bc49851384f5
Jihoon Yoo <jihoon.yoo@utah.edu>
2017-09-26 11:46:36 -0600

    Update index.js

--- index.js
+++ index.js
@@ -34,23 +34,19 @@ var parser = parse({
       var n_incorrect = datum[70];
       var score = datum[71];
 
-      // this user has full response records
-      if ('1766264' === canvas_user_id) {
-
         // index 9 ~ 68 (30 questions), {'question', 'possible score'} ...
-        for (var i = 9; i < 69; i += 2) {
-          var responses = datum[i].split(',').map(function(response) {
-            return response.substring(0, response.indexOf(':'));
-          });
+      for (var i = 9; i < 69; i += 2) {
+        var responses = datum[i].split(',').map(function(response) {
+          return response.substring(0, response.indexOf(':'));
+        });
 
-          // console.log(questions[(i - 9) / 2], '>>>', responses);
-          wstream.write('\n');
-          responses.forEach(function(response, index) {
-            var record = [canvas_user_id, questions[(i - 9) / 2], index + 1, response].join();
-            wstream.write(record + '\n');
-            // console.log(record);
-          })
-        }
+        // console.log(questions[(i - 9) / 2], '>>>', responses);
+        wstream.write('\n');
+        responses.forEach(function(response, index) {
+          var record = [canvas_user_id, questions[(i - 9) / 2], index + 1, response].join();
+          wstream.write(record + '\n');
+          // console.log(record);
+        })
       }
     };
   });
@@ -62,4 +58,4 @@ var wstream = fs.createWriteStream('final-report.csv');
 
 wstream.write(['canvas_user_id', 'canvas_quiz_question_id', 'quetion_number', 'user_response'].join() + '\n');
 
-fs.createReadStream('Quiz Student Analysis Report.csv').pipe(parser);
+fs.createReadStream('SOM SJT Assessment Exam Quiz Student Analysis Report.csv').pipe(parser);
